02689a6c6ae7881d1ab5adb93cda9814ba96d722
myyusuf <myyusuf1911@gmail.com>
2016-11-17 01:20:46 +0700

    Fix member list

--- app/handlers/member_data.js
+++ app/handlers/member_data.js
@@ -1,13 +1,32 @@
 exports.list = function(req, res, db) {
 
-  var query = "SELECT * FROM tb_member ";
+  var startIndexStr = req.query.startIndex;
+  var limitStr = req.query.limit;
 
-  db.query(
-    query, [],
-    function(err, rows) {
-      if (err) throw err;
-      res.json(rows);
-    }
-  );
+  if((startIndexStr != undefined && startIndexStr != '') && (limitStr != undefined && limitStr != '')){
+
+    var startIndex = parseInt(startIndexStr);
+    var limit = parseInt(limitStr);
+
+    var query = "SELECT * FROM member ORDER BY first_name LIMIT ?,? ";
+
+    db.query(
+      query, [startIndex, limit],
+      function(err, rows) {
+        if (err) throw err;
+        res.json(rows);
+      }
+    );
+  }else{
+    var query = "SELECT * FROM member ORDER BY first_name ";
+
+    db.query(
+      query, [],
+      function(err, rows) {
+        if (err) throw err;
+        res.json(rows);
+      }
+    );
+  }
 
 };
