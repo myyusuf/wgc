2fdb6cee24bf21aaddf680dcbad892f7195118cf
myyusuf <myyusuf1911@gmail.com>
2016-11-17 11:27:52 +0700

    Add member filter by name

--- app/handlers/member_data.js
+++ app/handlers/member_data.js
@@ -2,26 +2,33 @@ exports.list = function(req, res, db) {
 
   var startIndexStr = req.query.startIndex;
   var limitStr = req.query.limit;
+  var nameLike = req.query.nameLike;
+
+  if(nameLike == undefined){
+    nameLike = "%%"
+  }else{
+    nameLike = "%" + nameLike + "%"
+  }
 
   if((startIndexStr != undefined && startIndexStr != '') && (limitStr != undefined && limitStr != '')){
 
     var startIndex = parseInt(startIndexStr);
     var limit = parseInt(limitStr);
 
-    var query = "SELECT * FROM member ORDER BY first_name LIMIT ?,? ";
+    var query = "SELECT * FROM member WHERE first_name LIKE ? ORDER BY first_name LIMIT ?,? ";
 
     db.query(
-      query, [startIndex, limit],
+      query, [nameLike, startIndex, limit],
       function(err, rows) {
         if (err) throw err;
         res.json(rows);
       }
     );
   }else{
-    var query = "SELECT * FROM member ORDER BY first_name ";
+    var query = "SELECT * FROM member WHERE first_name LIKE ? ORDER BY first_name ";
 
     db.query(
-      query, [],
+      query, [nameLike],
       function(err, rows) {
         if (err) throw err;
         res.json(rows);
