e5b94fca8d1fb6ad8ba935e1894df7c4a5503cc6
myyusuf <myyusuf1911@gmail.com>
2016-12-01 06:51:34 +0700

    Add server directories

--- config/wgc_constant.js
+++ config/wgc_constant.js
@@ -1,8 +1,13 @@
 module.exports = {
 
-	'AVATAR_DIRECTORY_PATH' : '/Users/myyusuf/Documents/Test/upload_file/avatar/',
-	'PROMOTION_DIRECTORY_PATH' : '/Users/myyusuf/Documents/Test/wgcircle_data/promotion_images/',
-	'PRODUCT_DIRECTORY_PATH' : '/Users/myyusuf/Documents/Test/wgcircle_data/products/',
-	'NEWS_DIRECTORY_PATH' : '/Users/myyusuf/Documents/Test/wgcircle_data/news/'
+	// 'AVATAR_DIRECTORY_PATH' : '/Users/myyusuf/Documents/Test/upload_file/avatar/',
+	// 'PROMOTION_DIRECTORY_PATH' : '/Users/myyusuf/Documents/Test/wgcircle_data/promotion_images/',
+	// 'PRODUCT_DIRECTORY_PATH' : '/Users/myyusuf/Documents/Test/wgcircle_data/products/',
+	// 'NEWS_DIRECTORY_PATH' : '/Users/myyusuf/Documents/Test/wgcircle_data/news/'
+
+	'AVATAR_DIRECTORY_PATH' : '/var/upload_file/avatar/',
+	'PROMOTION_DIRECTORY_PATH' : '/var/wgcircle_data/promotion_images/',
+	'PRODUCT_DIRECTORY_PATH' : '/var/wgcircle_data/products/',
+	'NEWS_DIRECTORY_PATH' : '/var/wgcircle_data/news/'
 
 };
